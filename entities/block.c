453f845b45efa30b429595fc0ea3327a0299befc
ReimarPB <reimarpb@gmail.com>
2022-12-29 17:46:09 +0100

    Try some alternative physics

--- entities/block.c
+++ entities/block.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 #include "ball.h"
 #include "block.h"
 #include "../globals.h"
@@ -14,6 +16,33 @@ void standard_collision_checker(struct ball *ball, int block_x, int block_y)
 	int block_y_start = block_y;
 	int block_y_end = block_y + BLOCK_SIZE;
 
+	if (
+		ball_x_end >= block_x_start && ball_x_start <= block_x_end &&
+		ball_y_end >= block_y_start && ball_y_start <= block_y_end
+	) {
+		int diff_x = ball_x_start - block_x_start;
+		int diff_y = ball_y_start - block_y_start;
+
+		printf("%d %d\n", diff_x, diff_y);
+
+		if (abs(diff_x) >= abs(diff_y)) {
+			if (diff_x >= 0) ball->x = block_x_end + 1;
+			else ball->x = block_x_start - BALL_SIZE;
+			ball->x_vel = 1.0;
+		}
+
+		if (abs(diff_y) >= abs(diff_x)) {
+			if (diff_y >= 0) {
+				ball->y = block_y_end + 1;
+				ball->y_vel = 2.0;
+			} else {
+				ball->y = block_y_start - BALL_SIZE;
+				ball->y_vel = -2.0;
+			}
+		}
+	}
+
+	return;
 	// Check collisions
 	if (ball_y_start >= block_y_start && ball_y_start <= block_y_end) {
 		// from left

32b2a2cdde7075dbbe0317480f355a9b19f0596e
ReimarPB <reimarpb@gmail.com>
2023-03-29 19:21:47 +0200

    Fix warnings

--- scenes/pause.h
+++ scenes/pause.h
@@ -1,8 +1,10 @@
+#include "../native/common.h"
+
 void pause();
 
 void update_pause();
 
-void draw_pause();
+void draw_pause(int x, int y, int width, int height);
 
 void pause_onkeydown(enum key key);
 

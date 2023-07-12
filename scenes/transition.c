64fb95085ab4b9ff1b8c9b2330c5e3302172f1ec
ReimarPB <reimarpb@gmail.com>
2023-07-13 00:10:10 +0200

    Add chunks to transition effect

--- scenes/transition.c
+++ scenes/transition.c
@@ -7,6 +7,9 @@
 #include "scene.h"
 
 #define TRANSITION_SPEED 15
+#define TRANSITION_CHUNK_WIDTH 70
+#define TRANSITION_CHUNK_MAX_HEIGHT 50
+#define TRANSITION_CHUNK_AMOUNT (WIDTH_PX / TRANSITION_CHUNK_WIDTH)
 
 enum scene old_scene, new_scene;
 
@@ -15,12 +18,22 @@ enum scene old_scene, new_scene;
 // When it reaches the negative window height, the transition is done
 int progress;
 
+int chunk_offsets[TRANSITION_CHUNK_AMOUNT];
+
 void transition_to_scene(enum scene new)
 {
 	old_scene = scene;
 	new_scene = new;
 	scene = SCENE_TRANSITION;
+
 	progress = HEIGHT_PX;
+
+	// Generate offsets
+	for (int i = 0; i < TRANSITION_CHUNK_AMOUNT; i++) {
+		do {
+			chunk_offsets[i] = rand() % TRANSITION_CHUNK_MAX_HEIGHT;
+		} while (i > 0 && abs(chunk_offsets[i] - chunk_offsets[i-1]) < 10); // Ensure enough height difference between chunks
+	}
 }
 
 void update_transition()
@@ -32,7 +45,16 @@ void update_transition()
 		init_scene(new_scene);
 	}
 
-	if (progress == -HEIGHT_PX) scene = new_scene;
+	if (progress == -HEIGHT_PX) {
+		scene = new_scene;
+		redraw_area(
+			0,
+			HEIGHT_PX - TRANSITION_CHUNK_MAX_HEIGHT - TRANSITION_SPEED,
+			WIDTH_PX,
+			TRANSITION_CHUNK_MAX_HEIGHT + TRANSITION_SPEED
+		);
+		return;
+	}
 
 	redraw_area(0, abs(progress + TRANSITION_SPEED), WIDTH_PX, TRANSITION_SPEED);
 }
@@ -45,10 +67,20 @@ void draw_transition(int x, int y, int width, int height)
 	int black_start  = progress > 0 ? 0 : -progress;
 	int black_height = progress > 0 ? HEIGHT_PX - progress : HEIGHT_PX + progress;
 
+	// Draw scene area
 	scene = progress > 0 ? old_scene : new_scene;
 	draw(0, scene_start, WIDTH_PX, scene_height);
 	scene = SCENE_TRANSITION;
 
-	draw_rect(rgb(0, 0, 0), 0, black_start, WIDTH_PX, black_height);
+	// Draw black transition area
+	for (int i = 0; i < TRANSITION_CHUNK_AMOUNT; i++) {
+		draw_rect(
+			rgb(0, 0, 0),
+			i * TRANSITION_CHUNK_WIDTH,
+			progress > 0 ? black_start : black_start - chunk_offsets[i],
+			TRANSITION_CHUNK_WIDTH,
+			progress > 0 ? black_height + chunk_offsets[i] : black_height
+		);
+	}
 }
 
